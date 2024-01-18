bce90e8b622447ecca019449cc7b07730260a42d
Tyrese <tyresedieudonne1@gmail.com>
2024-01-18 02:27:27 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import GetApp from "@/components/GetApp";
+
+
+export default function Home() {
+  return (
+<>
+ <Hero />
+ <Camp />
+ <Guide />
+ <Features />
+ <GetApp />
+</>
+
+  );
+}
